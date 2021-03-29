164686b15805a127328d5b478ec00cfc97239731
justcoddev <edisonpaulcrz@gmail.com>
2021-03-29 11:21:08 -0500

    Agregé components header, body, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './PaginaPrincipal/header/header.component';
+import { BodyComponent } from './PaginaPrincipal/body/body.component';
+import { FooterComponent } from './PaginaPrincipal/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, BodyComponent, FooterComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
